3dfc7ddb4dca1e07831302d9fa2848c7949eb5bd
andreik <gs1994@gmail.com>
2018-05-15 19:40:22 +0300

    clean up tests

--- src/syntax/test/tokenizer.test.ts
+++ src/syntax/test/tokenizer.test.ts
@@ -1,12 +1,7 @@
 import { tokenize } from "../tokenizer";
 
 test('tokenizer', () => {
-
   const tokens = tokenize('слово , \n\rслово\n');
-
-  console.log(JSON.stringify(tokens, undefined, 2));
-
   expect(tokens.length).toEqual(7);
-
 });
 
