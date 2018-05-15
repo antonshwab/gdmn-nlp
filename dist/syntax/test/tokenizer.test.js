d55b1aac6ada29e7fdedfb40171e2eb6a3e65a9e
andreik <gs1994@gmail.com>
2018-05-15 15:22:23 +0300

    wip

--- dist/syntax/test/tokenizer.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tokenizer_1 = require("../tokenizer");
-test('tokenizer', () => {
-    const tokens = tokenizer_1.tokenize('слово , \n\rслово\n');
-    console.log(JSON.stringify(tokens, undefined, 2));
-    expect(tokens.length).toEqual(7);
-});
-//# sourceMappingURL=tokenizer.test.js.map
